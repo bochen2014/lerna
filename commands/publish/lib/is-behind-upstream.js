ae640f4c882e2ffbbe1cfbea1325f3a4369b1eec
Bo Chen <bo.chen@westpac.com.au>
2018-05-02 14:08:34 +1000

    git rev-list --left-right --count HEAD...origin/$(git rev-parse --abbrev-ref HEAD)

--- commands/publish/lib/is-behind-upstream.js
+++ commands/publish/lib/is-behind-upstream.js
@@ -8,9 +8,16 @@ module.exports = isBehindUpstream;
 function isBehindUpstream(gitRemote, branch, opts) {
   log.silly("isBehindUpstream");
 
-  updateRemote(opts);
+  updateRemote(opts); // git remote update (fetch all branches .etc)
 
   const remoteBranch = `${gitRemote}/${branch}`;
+  // symmetric difference ${sha1}...${sha2}
+  // == git rev-list sha1 sha2 --not $(git merge-base --all sha1 sha2)
+  // i.e. can be reached by either sha1 or sha2, but can't be reached by both;
+  //      effectivly that who sha1 difference from sha2
+
+  // here we are doing git rev-list --left-right  master...origin/master (exclude the merge-base of master and origin/master)
+  // i.e symetric difference between you current branch with origin/branch
   const [behind, ahead] = countLeftRight(`${remoteBranch}...${branch}`, opts);
 
   log.silly(
@@ -29,7 +36,7 @@ function updateRemote(opts) {
 function countLeftRight(symmetricDifference, opts) {
   const stdout = childProcess.execSync(
     "git",
-    ["rev-list", "--left-right", "--count", symmetricDifference],
+    ["rev-list", "--left-right", "--count", symmetricDifference], // git rev-list --left-right --count tells you statistics of how many commits you are behind , and how many ahead of
     opts
   );
 
